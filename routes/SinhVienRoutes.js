12a60ed0b961f731c78d6e8dd01c9c7e4a3e1c8d
LHT <lamhuetrung@gmail.com>
2024-05-07 22:45:50 +0700

    Xây dựng API lấy danh sách tin tức

--- routes/SinhVienRoutes.js
+++ routes/SinhVienRoutes.js
@@ -2,20 +2,15 @@ const express = require('express');
 const router = express.Router();
 const SinhvienController = require('../controllers/SinhVienController');
 const TintucController = require('../controllers/TinTucController');
-const ThuctapController = require('../controllers/ThucTapController');
 
 router.get('/tintuc', TintucController.layThongBao);
-router.get('/danhsachsinhvien', SinhvienController.layDanhSachSinhVien);
-router.get('/donthuctap', ThuctapController.layDanhSachThuctap);
 
 router.post('/themthongtin', SinhvienController.themThongTinSinhVien);
 router.post('/themthongbao', TintucController.themThongBao);
-router.post('/dangkythuctap', ThuctapController.DangKyThucTap);
 
 router.put(
     '/capnhattrangthai/:SinhVienID',
     SinhvienController.TrangThaiSinhVien,
 );
-router.put('/xoadonthuctap/:DTTID', SinhvienController.xoadonthuctap);
 
 module.exports = router;
