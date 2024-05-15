a66fe47d0bd060acdc830e603448387c539da4f4
Hoangnhut0 <hoangnhutnguyen7@gmail.com>
2024-05-15 18:31:45 +0700

    code tạo giao diện Tin tức, Sinh viên, Giáo viên, Đợt thực tập và Thêm đợt thực tập cho Admin

--- src/App.js
+++ src/App.js
@@ -14,8 +14,59 @@ import Tintuc from '../src/pages/Student/tintuc';
 import TintucAD from '../src/pages/Admin/tintuc';
 import TintucGV from '../src/pages/Teacher/tintuc';
 
+import DonThuctap from '../src/pages/Student/dondangky';
+import DonThucTapGV from '../src/pages/Teacher/quanlythuctap';
+import ThongTinDangKyGV from '../src/pages/Teacher/thongtindangky';
+import ThongTinGV from '../src/pages/Teacher/dangkythongtin';
+
+import Thuctap from '../src/pages/Student/thuctap';
+import SinhvienDangKyGV from '../src/pages/Teacher/sinhvien';
+import XemBaoCao from '../src/pages/Teacher/xembaocao';
+
+import Sinhvien from '../src/pages/Admin/sinhvien';
+import Giaovien from '../src/pages/Admin/giaovien';
+import DotThucTap from './pages/Admin/dotthuctap';
+import QuanLyDotThucTap from './pages/Admin/quanlydotthuctap';
+import ThemDotThucTap from './pages/Admin/themdulieuthuctap';
+import SuaDotThucTap from './pages/Admin/thaydoidulieuthuctap';
 
 const router = createBrowserRouter([
+    {
+        path: '/admin/quanlydotthuctap/:slug',
+        element: <DotThucTap />,
+    },
+    {
+        path: '/admin/themdotthuctap/:slug',
+        element: <ThemDotThucTap />,
+    },
+    {
+        path: '/admin/thaydoidotthuctap/:slug',
+        element: <SuaDotThucTap />,
+    },
+    {
+        path: '/admin/danhsachdotthuctap/:slug',
+        element: <QuanLyDotThucTap />,
+    },
+    {
+        path: '/admin/giaovien/:slug',
+        element: <Giaovien />,
+    },
+    {
+        path: '/admin/sinhvien/:slug',
+        element: <Sinhvien />,
+    },
+    {
+        path: '/teacher/sinhvien/:slug',
+        element: <SinhvienDangKyGV />,
+    },
+    {
+        path: '/student/thuctap/:slug',
+        element: <Thuctap />,
+    },
+    {
+        path: '/student/dondangky/:slug',
+        element: <DonThuctap />,
+    },
     {
         path: '/student/tintuc/:slug',
         element: <Tintuc />,
@@ -23,6 +74,22 @@ const router = createBrowserRouter([
     {
         path: '/admin/tintuc/:slug',
         element: <TintucAD />,
+    },
+    {
+        path: '/teacher/quanlythuctap/:slug',
+        element: <DonThucTapGV />,
+    },
+    {
+        path: '/teacher/quanlythuctap/thongtindangky/:slug',
+        element: <ThongTinDangKyGV />,
+    },
+    {
+        path: '/teacher/quanlythuctap/xembaocao/:slug',
+        element: <XemBaoCao />,
+    },
+    {
+        path: '/teacher/thongtin/:slug',
+        element: <ThongTinGV />,
     },
     {
         path: '/teacher/tintuc/:slug',
